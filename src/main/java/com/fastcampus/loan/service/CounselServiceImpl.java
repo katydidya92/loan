fdfbc55f74cf79b0be579768ec6ef3c23be7f847
katydidya92 <sweets1327@gmail.com>
2023-01-16 19:49:13 +0900

    get / update / delete 추가

--- src/main/java/com/fastcampus/loan/service/CounselServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/CounselServiceImpl.java
@@ -3,12 +3,13 @@ package com.fastcampus.loan.service;
 import com.fastcampus.loan.domain.Counsel;
 import com.fastcampus.loan.dto.CounselDTO.Request;
 import com.fastcampus.loan.dto.CounselDTO.Response;
+import com.fastcampus.loan.exception.BaseException;
+import com.fastcampus.loan.exception.ResultType;
 import com.fastcampus.loan.repository.CounselRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -28,4 +29,42 @@ public class CounselServiceImpl implements CounselService {
         return modelMapper.map(created, Response.class);
     }
 
+    @Override
+    public Response get(Long counselId) {
+        Counsel counsel = counselRepository.findById(counselId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        return modelMapper.map(counsel, Response.class);
+    }
+
+    @Override
+    public Response update(Long counselId, Request request) {
+        Counsel counsel = counselRepository.findById(counselId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        counsel.setName(request.getName());
+        counsel.setCellPhone(request.getCellPhone());
+        counsel.setEmail(request.getEmail());
+        counsel.setMemo(request.getMemo());
+        counsel.setAddress(request.getAddress());
+        counsel.setAddressDetail(request.getAddressDetail());
+        counsel.setZipCode(request.getZipCode());
+
+        counselRepository.save(counsel);
+
+        return modelMapper.map(counsel, Response.class);
+    }
+
+    @Override
+    public void delete(Long counselId) {
+        Counsel counsel = counselRepository.findById(counselId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        counsel.setIsDeleted(true);
+
+        counselRepository.save(counsel);
+    }
 }
